a7d0236ff08843f552dcde27adef0a40621186e2
dmtrth25 <vodolazhchenko25@gmail.com>
2023-05-29 15:56:43 +0300

    created create page styles

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -4,8 +4,28 @@ import Link from 'next/link';
 const Page: NextPage = () => {
   return (
     <>
-      <div>Page</div>
-      <Link href="/">Back</Link>
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-2xl">Create Page</h1>
+      </div>
+      <form className="flex flex-col gap-1">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent px-2 py-2 rounded-md outline-none focus-within:bg-slate-700 transition-all duration-500"
+        />
+        <div className="flex justify-end gap-2">
+          <Link
+            href="/"
+            className="border border-slate-300 px-2 py-1 rounded-md hover:bg-slate-500 transition-all duration-300">
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 px-2 py-1 rounded-md hover:bg-slate-500 transition-all duration-300">
+            Create
+          </button>
+        </div>
+      </form>
     </>
   );
 };
